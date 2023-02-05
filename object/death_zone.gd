95cc197b6145cc722550223c153267365e319a96
Dylan <95886475+melo484b@users.noreply.github.com>
2023-02-04 20:16:31 -0500

    Implement clean up signals for falling blocks

--- object/death_zone.gd
+++ object/death_zone.gd
@@ -2,8 +2,11 @@ extends Area2D
 
 
 signal death
+signal clean_up
 
 
-func _on_death_area_area_entered(area):
+func _on_death_area_area_entered(area) -> void:
 	if area.is_in_group("PLAYER"):
 		emit_signal("death")
+	if area.is_in_group("BLOCK"):
+		emit_signal("clean_up")
