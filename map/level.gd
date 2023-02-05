739a8a998ff1719e1d045104c6ef99e9fd943f2c
Dylan <95886475+melo484b@users.noreply.github.com>
2023-02-04 22:07:15 -0500

    Implement game start and end

--- map/level.gd
+++ map/level.gd
@@ -1,14 +1,34 @@
 extends Node2D
 
 
+const PLAYER_START_POS: Vector2 = Vector2(498, 530)
+
+var player: PackedScene = preload("res://player/player.tscn")
+
 onready var spawn_manager: Node = $spawn_manager
 onready var level_movement_manager: Node2D = $level_movement_manager
+onready var ui: Control = $UI
 
 
-func _on_spawn_manager_spawning():
+func new_game() -> void:
+	Scorekeeper.score = 0
+	spawn_player()
+	level_movement_manager.reset()
+
+
+func spawn_player() -> void:
+	var new_player = player.instance()
+	new_player.position = PLAYER_START_POS
+	add_child(new_player)
+
+func _on_spawn_manager_spawning() -> void:
 	spawn_manager.spawn_block()
 
 
 # TODO: End the game
-func _on_death_area_death():
-	pass # Replace with function body.
+func _on_death_area_death() -> void:
+	ui.game_over()
+
+
+func _on_UI_start_game() -> void:
+	new_game()
